d5492d26f8e8001770bf14277d86ef84da09c711
Nithin Adidela <nithinadidela@gmail.com>
2025-03-28 20:59:55 -0700

    add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+#------------------------------------------------------------------------------- 
+Filename:         main.py
+Description:      main file for the project
+Author:           Nithin Adidela
+Date Created:     2025 March 28
+Last Modified:    2025 March 28
+#------------------------------------------------------------------------------- 
+"""
+
