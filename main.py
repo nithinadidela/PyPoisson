8ba7ca80186bc52b067552a4d245c287e2d8a550
Nithin Adidela <nithinadidela@gmail.com>
2025-04-02 18:18:16 -0700

    opt jupyter notebook style main.ipynb; add latex problem description and benchmark

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ Last Modified:    2025 March 28
 #------------------------------------------------------------------------------- 
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+import grid2D
+
+L = 1.
+N = 8
+
+X, Y = grid2D.grid2D(L, N)
+
+Uref = np.sin(np.pi * X) * np.cos(np.pi * Y)
+f = -2 * np.pi**2 * np.sin(np.pi * X) * np.cos(np.pi * Y)
+
+plt.contourf(X, Y, Uref, 50, cmap='viridis')
+plt.colorbar()
+plt.title("Exact Solution to Poisson Equation (Dirichlet BCs)")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
